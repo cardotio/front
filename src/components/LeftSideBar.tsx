0f9d646016d79b7d70e98f2b6dbfe0f88d1122d5
jooyul-yoon <juyeolyoon@gmail.com>
2022-10-22 13:57:48 +0000

    (style) 221022 - left sidebar member

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -37,6 +37,7 @@ const DropDown = styled(motion.div)`
   box-shadow: rgb(15 15 15 / 5%) 0px 0px 0px 1px,
     rgb(15 15 15 / 10%) 0px 3px 6px, rgb(15 15 15 / 20%) 0px 9px 24px;
   overflow-y: auto;
+  z-index: 10;
 `;
 
 interface LeftBarProps {
--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { TypeMember } from 'types';
+import { FcBusinessman } from 'react-icons/fc';
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,6 +11,8 @@ const Wrapper = styled.div`
   width: 100%;
 `;
 const MemberContainer = styled.div`
+  display: flex;
+  align-items: center;
   width: 100%;
   height: 56px;
   margin-bottom: 10px;
@@ -26,6 +29,55 @@ const MemberContainer = styled.div`
     background: #d8d8d8;
   }
 `;
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 45px;
+  height: 45px;
+  margin-right: 5px;
+  border-radius: 45px;
+  border: 1px solid black;
+  svg {
+    width: 45px;
+    height: 45px;
+  }
+`;
+const MemberInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 5px;
+  border-bottom: 1px solid black;
+`;
+const Description = styled.div`
+  font-family: 'IBM Plex Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 13px;
+`;
+const DisplayName = styled.div`
+  font-family: 'IBM Plex Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 13px;
+  line-height: 17px;
+`;
+const Role = styled.div`
+  height: fit-content;
+  transform: translateY(6px);
+  font-family: 'IBM Plex Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 7px;
+  line-height: 9px;
+`;
+
 interface props {
   teamname: string;
 }
@@ -33,8 +85,6 @@ interface props {
 function Members({ teamname }: props) {
   const [myTeams, setMyTeams] = useRecoilState(myTeamsAtom);
   const [users, setUsers] = useState<TypeMember[]>([]);
-
-  console.log(myTeams.filter((team) => team.teamname === teamname));
 
   useEffect(() => {
     myTeams.filter((team) => team.teamname === teamname).length > 0 &&
@@ -45,8 +95,16 @@ function Members({ teamname }: props) {
     <Wrapper>
       {users?.map((user, i) => (
         <MemberContainer key={i}>
-          <div>{user.displayname}</div>
-          <div>{user.role}</div>
+          <ImageContainer>
+            <FcBusinessman />
+          </ImageContainer>
+          <MemberInfo>
+            <Header>
+              <DisplayName>{user.username}</DisplayName>
+              <Role>{user.role}</Role>
+            </Header>
+            <Description>{user.email}</Description>
+          </MemberInfo>
         </MemberContainer>
       ))}
     </Wrapper>
